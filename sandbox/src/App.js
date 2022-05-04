89f384d5bfef9e521ba1cd9d405b3d2ad41809f3
Cody Burleson <cody.burleson@gmail.com>
2022-05-03 19:53:48 -0600

    2022-05-03 7:53 PM

---
+++ sandbox/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            {/*
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            */}
+            
+            <div class="workspace-leaf-content" data-type="collection-manager">
+                <div class="view-content"><h4>Hello, React!</h4></div>
+            </div>
+
+
+
+        </div>
+    );
+}
+
+export default App;
